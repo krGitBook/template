0947601f51cd8c1265a669a307d8d2a7c2196b4c
yihaoliu <1453961977@qq.com>
2018-05-29 23:30:24 +0800

    基本框架

---
+++ template/page.vue
@@ -0,0 +1,13 @@
+<template>
+  <FloorPlan ></FloorPlan>
+</template>
+
+<script>
+import FloorPlan from '~/components/FloorPlan/index'
+// import pdfurl from '~/plugins/Images.pdf'
+export default {
+  components: {
+    FloorPlan
+  }
+}
+</script>
